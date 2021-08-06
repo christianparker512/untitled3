a877a9ebc2421cd35b2d86b5fa865f660c738c68
Christian Parker <christianparker512@gmail.com>
2021-08-06 14:02:35 -0500

    push pop shift

--- js/push.js
+++ js/push.js
@@ -1,5 +1,17 @@
 'use strict';
 
+//push adds it to the end of the array
 const colors = ['red', 'blue', 'yellow'];
 
+colors.push('orange');
 console.log(colors);
+
+//removes the last thing in the array
+const parkers = ['chris', 'abby', 'lisa'];
+parkers.pop();
+console.log(parkers);
+
+//shift removes the first
+const bobs = ['matt', 'rob', 'brad', 'trace','chris'];
+bobs.shift();
+console.log(bobs)
